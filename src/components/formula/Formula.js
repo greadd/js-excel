5105cdb3881c1ac7d32761cd38f13754ca4c67a7
Gleb Yakovlev <gleb.grd@gmail.com>
2020-10-11 00:44:00 +0300

    Finish base table logic

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,12 +1,14 @@
 import { ExcelComponent } from '@core/ExcelComponent'
+import { $ } from '@core/dom'
 
 export class Formula extends ExcelComponent {
   static className = 'excel__formula'
 
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
       name: 'Formula',
-      listeners: ['input', 'click']
+      listeners: ['input', 'keydown'],
+      ...options
     })
   }
 
@@ -15,17 +17,33 @@ export class Formula extends ExcelComponent {
   <div class="info">
     fx
   </div>
-  <div class="input" contenteditable spellcheck="false">
+  <div id="formula" class="input" contenteditable spellcheck="false">
 
   </div>
     `
   }
 
-  onInput(event) {
-    console.log('Formula onInput', event)
+  init() {
+    super.init()
+
+    this.$formula = this.$root.find('#formula')
+
+    this.$on('table:select', $cell => {
+      this.$formula.text($cell.text())
+    })
+
+    this.$on('table:input', $cell => this.$formula.text($cell.text()))
   }
 
-  onClick(event) {
-    console.log('Formula onClick', event)
+  onInput(event) {
+    this.$emit('formula:input', $(event.target).text())
+  }
+
+  onKeydown(event) {
+    const keys = ['Tab', 'Enter']
+    if (keys.includes(event.key)) {
+      event.preventDefault()
+      this.$emit('formula:done')
+    }
   }
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,10 +3,24 @@ const CODES = {
   Z: 90
 }
 
-function toCell(_, index) {
-  return `
-    <div class="cell" contentEditable data-col="${index}"></div>
-  `
+// function toCell(_, index) {
+//   return `
+//     <div class="cell" contentEditable data-col="${index}"></div>
+//   `
+// }
+
+function toCell(row) {
+  return function(_, col) {
+    return `
+        <div
+        class="cell"
+        contentEditable
+        data-col="${col}"
+        data-type="cell"
+        data-id="${row}:${col}"
+        ></div>
+      `
+  }
 }
 
 function toColumn(col, index) {
@@ -43,10 +57,13 @@ export function createTable(rowsCount = 15) {
 
   rows.push(createRow(null, cols))
 
-  for (let i = 0; i < rowsCount; i++) {
-    const cells = new Array(colsCount).fill('').map(toCell).join('')
+  for (let row = 0; row < rowsCount; row++) {
+    const cells = new Array(colsCount)
+      .fill('')
+      .map(toCell(row))
+      .join('')
 
-    rows.push(createRow(i + 1, cells))
+    rows.push(createRow(row + 1, cells))
   }
 
   return rows.join('')
